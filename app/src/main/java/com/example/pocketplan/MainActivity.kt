fc31077eb3d3ef932ee10ea77224812dd06054a3
ST10290748 <126698053+maanie-waanie@users.noreply.github.com>
2025-04-14 14:16:58 +0200

    Commiting the Home Page and Register Page

--- app/src/main/java/com/example/pocketplan/MainActivity.kt
+++ app/src/main/java/com/example/pocketplan/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.pocketplan
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var loginButton: Button
+    private lateinit var registerButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        loginButton = findViewById(R.id.loginButton)
+        registerButton = findViewById(R.id.registerButton)
+
+        loginButton.setOnClickListener {
+            // Navigate to LoginActivity
+//            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        registerButton.setOnClickListener {
+            // Navigate to RegisterActivity
+//            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
